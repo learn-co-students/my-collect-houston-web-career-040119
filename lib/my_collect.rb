b0b76eb6c761212acf23ec8fc2f4c79b8990feef
Jusitn Avioli <justin.avioli@gmail.com>
2019-03-27 17:50:35 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,18 @@
+def my_collect(collection)
+  counter = 0
+  new_collection = []
+  if collection == [] 
+    new_collection = []
+  else 
+    collection_count = collection.size  
+    while counter < collection_count
+      new_value = yield collection[counter]
+      new_collection << new_value
+      counter += 1
+    end
+  end
+  new_collection
+end
 
+my_collect("It Me") {|i| "#{i.upcase}"}
 
